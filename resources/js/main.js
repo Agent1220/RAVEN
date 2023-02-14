2ddaa461c04601855763395efd7d560dbfc2b2c6
Agent1220 <38262512+Agent1220@users.noreply.github.com>
2023-02-14 10:12:56 +0100

    Initial commit of V1.3.3
    -new: explosion effect when notes (incl. ends of LNs) land
    -mwheel scrolling change: no longer snaps audio (notes still snap). new rolling speed is x/10 seconds
    -removed selected note id field and jumping to selected id
    
    fixes:
    -fixed LN reverting
    -fixed LN body clearing
    
    patch .1:
    -fixed LN body fading on <^> directions
    
    patch .2:
    -fixed LN bodies not being made when you load a chart that has LNs
    
    patch .3
    -landing explosions now will only fire if the audio is actually playing
    -effects now also only update when the audio is playing, otherwise (eg. jumping to position via progress bar click) they get cleared up
    -added tolerance to effects so they properly start and finish their fades. the same tolerance applies to the explosion effects that I implemented when they came in. it's a global variable, if it fires more or less than 1 time during play you can adjust it to fit your performance (that could depend on the number of notes) or your refresh rate, or if effects leave a "mark"

---
+++ resources/js/main.js
@@ -0,0 +1,105 @@
+// See more details: https://neutralino.js.org/docs/how-to/use-a-frontend-library
+
+//CLASSES
+class Note{
+    constructor(time, tailId, direction, cell,snap){
+        this.time = time;
+        this.cell = cell;
+        this.direction = direction;
+        this.tailId = tailId;
+        //this.id = id;
+        this.snap = snap;
+    }
+}
+
+class Effect{
+    constructor(time, duration, direction, cell, opacity){
+        this.time = time;
+        this.duration = duration;
+        this.direction = direction;
+        this.cell = cell;
+        this.opacity = opacity;
+    }
+}
+
+class ChartMetadata{
+    constructor(songName, artist, genre, bpm, charter, style, scale){
+        this.songName = songName;
+        this.artist = artist;
+        this.bpm = bpm;
+        this.genre = genre;
+        this.charter = charter;
+        this.style = style;
+        this.scale = scale;
+    }
+}
+
+class ChartHead{
+    constructor(length, startingBpm, offset,noteCount,/*avgDensity,*/style){
+        this.length = length;
+        this.startingBpm = startingBpm;
+        this.offset = offset;
+        this.noteCount = noteCount;
+        //this.avgDensity = avgDensity;
+        this.style = style;;
+    }
+}
+
+class Chart{
+    constructor(meta, head, notes, effects){
+        this.meta = meta;
+        this.head = head;
+        this.notes = notes;
+        this.effects = effects;
+    }
+}
+
+//FUNCTIONS, VARIABLES
+var ctrlPressed = false;
+
+//this just makes sure none of the images are draggable
+let alIm = document.querySelectorAll("img");
+for (let iM of alIm){
+    iM.setAttribute("draggable", "false");
+}
+
+document.getElementById("body").addEventListener("wheel", (e)=>e.preventDefault(),{passive: false});
+
+window.onload = () => {
+    document.getElementById("body").style.display = "";
+    getEffectImages();
+    getCells();
+    document.getElementById("body").setAttribute("onresize","calculateGridPositions()")
+    document.getElementById("eDir").style.display = "none";
+    document.getElementById("eSettings").style.display = "none";
+    
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
+
+    setTimeout(calculateGridPositions, 1000);
+}
+
+function getMousePos(event){
+    return {x:event.clientX, y:event.clientY};
+}
+
+//NATIVE API
+Neutralino.init();
+
+async function toggleFullscreen(){
+    if (await Neutralino.window.isFullScreen()){
+        await Neutralino.window.exitFullScreen();
+    } else {
+        await Neutralino.window.setFullScreen();
+    }
+}
+
+//DO NOT DELETE THIS OR THE WINDOW WON'T CLOSE WITH THE CLOSE BUTTON
+//AND YOU'LL HAVE TO KILL IT IN TASK MANAGER.
+Neutralino.events.on("windowClose", () => {
+    closeWindow();
+});
+
+function closeWindow(){
+    Neutralino.app.exit();
+}
